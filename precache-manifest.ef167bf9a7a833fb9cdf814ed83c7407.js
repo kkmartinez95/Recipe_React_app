913c74e82751188c3f18289ab64e877253e7e827
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-06-19 23:16:45 -0400

    Updates

---
+++ precache-manifest.ef167bf9a7a833fb9cdf814ed83c7407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c0a22b5d35d8377d85c0b57eb05cc7",
+    "url": "/Recipe_React_app/index.html"
+  },
+  {
+    "revision": "bed2c911df78dc7009fd",
+    "url": "/Recipe_React_app/static/css/main.d06afda8.chunk.css"
+  },
+  {
+    "revision": "6348937e8e7d3fde5149",
+    "url": "/Recipe_React_app/static/js/2.28adb7db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe_React_app/static/js/2.28adb7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed2c911df78dc7009fd",
+    "url": "/Recipe_React_app/static/js/main.0f09cce7.chunk.js"
+  },
+  {
+    "revision": "62c6b02408b786fabadf",
+    "url": "/Recipe_React_app/static/js/runtime-main.fa10a867.js"
+  },
+  {
+    "revision": "2a816fbca7ec620aecde758689886a7b",
+    "url": "/Recipe_React_app/static/media/girl.2a816fbc.png"
+  }
+]);
